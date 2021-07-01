e07f672646a442064bcdd90fce95a8c7a80a9011
Oleg <yourwebpixel@gmail.com>
2021-07-01 18:38:33 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+// const   last = document.querySelector("start"),
+//         next = document.querySelector(".tsss");
+
+
+// Интерполяция
+const siteUrl = "page_01",
+      fullUrlSite = `https://someurl.com/${siteUrl}`,
+      fillUrlSite2 = "https://someurl.com/" + siteUrl;
+
+      
+console.log(fullUrlSite);
+
+// Использование Git
+/* 
+Инициализация Git
+git init
+
+Локальное применение настроек к конкретному проекту
+    git config --local user.name "Oleg"
+    git config --local user.email "yourwebpixel@gmail.com"
+
+Глобальное применение настроек
+    git config --global user.name "Oleg"
+    git config --global user.email "yourwebpixel@gmail.com"
+
+Проверка статуса
+    git status
+
+Добавление (всех "-А" или ".") файлов в состояние отслеживания
+    git add -A  или git add .
+Добавление определенного файла в состояние отслеживания
+    git add main.css
+Добавление файлов в коммит c подписью изменений
+    git commit -a -m"message"
+
+Просмотр логов комита
+    git log
+*/
+
+
+
+        
